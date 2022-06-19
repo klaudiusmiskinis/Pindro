bfa6486ae5995327f576a579f25c1e6cfa3e9f3f
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-06-19 17:43:27 +0200

    Añadiedo rutas para los assets

--- pindro.js
+++ pindro.js
@@ -8,12 +8,39 @@ const { v4: uuidV4 } = require("uuid");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+app.get("/assets/:file", (req, res) => {
+  res.sendFile(__dirname + "/views/assets/" + req.params.file);
+});
+
 app.get("/", (req, res) => {
+  res.redirect("/home");
+});
+
+app.get("/crear", (req, res) => {
   res.redirect(`/${uuidV4()}`);
 });
 
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+
+app.get("/:room", (req, res) => {
+  const render = {
+    roomId: req.params.room,
+  };
+  res.render("room", render);
+});
+
 io.on("connection", (socket) => {
-  console.log(socket);
+  console.log(socket.id);
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
+  });
 });
 
 server.listen(3000);
